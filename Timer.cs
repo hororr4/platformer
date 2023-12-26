72077881a79f927a5b3b7c88d6d6b40d3760b8f2
hororr4 <149958551+hororr4@users.noreply.github.com>
2023-12-26 19:29:11 +0300

    Add files via upload

---
+++ Timer.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public int minutes;
+    public float seconds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        seconds -= Timer.deltaTime;
+        if (seconds <= 0)
+        
+        {
+            if (minutes > 0)
+            {
+                seconds += 59;
+                minutes -= 1;
+            }
+            else
+            {
+                int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(sceneIndex);
+            }
+        }
+        int roundSeconds = Mathf.RoundToint(seconds);
+        timerText.text = minutes + ":" + roundSeconds;
+
+    }
+}
